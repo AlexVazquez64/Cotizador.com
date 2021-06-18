f0b7dd3c5ef361e9cbc09feac2c21642e2f607d2
Alex Vázquez <alejandrovazquezc@outlook.com>
2021-06-18 10:57:05 -0600

    Version 1.1.1 Cotizador

--- database/config.js
+++ database/config.js
@@ -1,26 +1,10 @@
 // Localhost
 
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "cotizacion",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-
-// Server
-
 // module.exports = {
-//   HOST: "mysql5032.site4now.net",
-//   USER: "a2b83a_cotiza",
-//   PASSWORD: "cotiza21#",
-//   DB: "db_a2b83a_cotiza",
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "cotizacion",
 //   dialect: "mysql",
 //   pool: {
 //     max: 5,
@@ -29,3 +13,19 @@ module.exports = {
 //     idle: 10000
 //   }
 // };
+
+// Server
+
+module.exports = {
+  HOST: "mysql5032.site4now.net",
+  USER: "a2b83a_cotiza",
+  PASSWORD: "cotiza21#",
+  DB: "db_a2b83a_cotiza",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
